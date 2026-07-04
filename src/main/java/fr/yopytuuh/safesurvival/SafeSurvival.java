05fa9b833975a83f7e75fd125093c191c0008a80
Yopytuuh <81307833+yopytuuh@users.noreply.github.com>
2026-07-05 01:36:48 +0200

    project start

---
+++ src/main/java/fr/yopytuuh/safesurvival/SafeSurvival.java
@@ -0,0 +1,16 @@
+package fr.yopytuuh.safesurvival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SafeSurvival extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
